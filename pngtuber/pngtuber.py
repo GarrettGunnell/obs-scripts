b7f49bf1ae559591350a91111fc8d9aa2a432ff1
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-03-27 22:01:23 -0700

    Cache user settings in global parameters

--- pngtuber/pngtuber.py
+++ pngtuber/pngtuber.py
@@ -1,26 +1,25 @@
 import obspython as obs
 import math, time
 
+# Parameters
+pngtuber_source = None
+idle_image_path = None
+
+
 # Description displayed in the Scripts dialog window
 def script_description():
-  return """
+  return \
+"""
 PNGTuber Animator
     by @Acerola_t
 
 Change your PNGTuber source based on any number of sound gates.
 """
 
-# Update (Called once per frame)
-def script_tick(seconds):
-    current_scene_as_source = obs.obs_frontend_get_current_scene()
-    if current_scene_as_source:
-        current_scene = obs.obs_scene_from_source(current_scene_as_source)
-        scene_item = obs.obs_scene_find_source_recursive(current_scene, "Dev Example")
+# Set Default Values
+def script_defaults(settings):
+    pass
 
-    if scene_item:
-        obs.obs_sceneitem_set_rot(scene_item, 10 * math.sin(12 * time.time()))
-
-    obs.obs_source_release(current_scene_as_source)
 
 # UI
 def script_properties():
@@ -29,7 +28,7 @@ def script_properties():
     p = obs.obs_properties_add_list(
         properties,
         "png source",
-        "PNG Source",
+        "PNG Source:",
         obs.OBS_COMBO_TYPE_LIST,
         obs.OBS_COMBO_FORMAT_STRING,
     )
@@ -47,4 +46,33 @@ def script_properties():
 
     obs.obs_properties_add_path(properties, "idle image path", "Idle Image Path:", obs.OBS_PATH_FILE, "All formats (*.bmp *.tga *.png *.jpeg *.jpg *.jxr *.gif *.psd *.webp);; BMP Files (*.bmp);; Targa Files (*.tga);; PNG Files (*.png);; JPEG Files (*.jpeg, *.jpg);; JXR Files (*.jxr);; GIF Files (*.gif);; PSD Files (*.psd);; WebP Files (*.webp);; All Files (*.*)", "C:/Pictures/")
 
+
+    obs.obs_properties_add_button(properties, "debug button", "Debug", debug_output)
+
     return properties
+
+
+# Cache GUI Parameters
+def script_update(settings):
+    global pngtuber_source, idle_image_path
+
+    pngtuber_source = obs.obs_data_get_string(settings, "png source")
+    idle_image_path = obs.obs_data_get_string(settings, "idle image path")
+
+
+# Update (Called once per frame)
+def script_tick(seconds):
+    current_scene_as_source = obs.obs_frontend_get_current_scene()
+    if current_scene_as_source:
+        current_scene = obs.obs_scene_from_source(current_scene_as_source)
+        scene_item = obs.obs_scene_find_source_recursive(current_scene, "Dev Example")
+
+    if scene_item:
+        obs.obs_sceneitem_set_rot(scene_item, 10 * math.sin(12 * time.time()))
+
+    obs.obs_source_release(current_scene_as_source)
+
+
+# Debug Button
+def debug_output(props, prop):
+    print(idle_image_path)
