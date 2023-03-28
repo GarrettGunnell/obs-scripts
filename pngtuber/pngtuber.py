4aeef161b6acc5f27deb01e56faf0367fe26eb02
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-03-27 20:41:52 -0700

    Add pngtuber script file and description

---
+++ pngtuber/pngtuber.py
@@ -0,0 +1,10 @@
+import obspython as obs
+
+# Description displayed in the Scripts dialog window
+def script_description():
+  return """
+PNGTuber Animator
+by @Acerola_t
+
+Change your PNGTuber source based on any number of sound gates.
+"""
