5139448742e2ca4f7b8a8f02aa621458dea6bbcf
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-03-27 22:38:51 -0700

    Swapping between two images poc

--- pngtuber/pngtuber.py
+++ pngtuber/pngtuber.py
@@ -1,10 +1,9 @@
 import obspython as obs
-import math, time
 
 # Parameters
 pngtuber_source = None
 idle_image_path = None
-
+talking_image_path = None
 
 # Description displayed in the Scripts dialog window
 def script_description():
@@ -46,35 +45,31 @@ def script_properties():
 
     obs.obs_properties_add_path(properties, "idle image path", "Idle Image Path:", obs.OBS_PATH_FILE, "All formats (*.bmp *.tga *.png *.jpeg *.jpg *.jxr *.gif *.psd *.webp);; BMP Files (*.bmp);; Targa Files (*.tga);; PNG Files (*.png);; JPEG Files (*.jpeg, *.jpg);; JXR Files (*.jxr);; GIF Files (*.gif);; PSD Files (*.psd);; WebP Files (*.webp);; All Files (*.*)", "C:/Pictures/")
 
+    obs.obs_properties_add_button(properties, "idle button", "Idle", idle)
 
-    obs.obs_properties_add_button(properties, "debug button", "Debug", debug_output)
+    obs.obs_properties_add_path(properties, "talking image path", "Talking Image Path:", obs.OBS_PATH_FILE, "All formats (*.bmp *.tga *.png *.jpeg *.jpg *.jxr *.gif *.psd *.webp);; BMP Files (*.bmp);; Targa Files (*.tga);; PNG Files (*.png);; JPEG Files (*.jpeg, *.jpg);; JXR Files (*.jxr);; GIF Files (*.gif);; PSD Files (*.psd);; WebP Files (*.webp);; All Files (*.*)", "C:/Pictures/")
+
+    obs.obs_properties_add_button(properties, "talking button", "Talk", talk)
 
     return properties
 
 
 # Cache GUI Parameters
 def script_update(settings):
-    global pngtuber_source, idle_image_path
+    global pngtuber_source, idle_image_path, talking_image_path
 
     pngtuber_source = obs.obs_data_get_string(settings, "png source")
     idle_image_path = obs.obs_data_get_string(settings, "idle image path")
+    talking_image_path = obs.obs_data_get_string(settings, "talking image path")
 
 
 # Update (Called once per frame)
 def script_tick(seconds):
-    current_scene_as_source = obs.obs_frontend_get_current_scene()
-    if current_scene_as_source:
-        current_scene = obs.obs_scene_from_source(current_scene_as_source)
-        scene_item = obs.obs_scene_find_source_recursive(current_scene, "Dev Example")
-
-    if scene_item:
-        obs.obs_sceneitem_set_rot(scene_item, 10 * math.sin(12 * time.time()))
-
-    obs.obs_source_release(current_scene_as_source)
+    pass
 
 
 # Debug Button
-def debug_output(props, prop):
+def idle(props, prop):
     if not pngtuber_source:
         print("Select your PNGTuber source")
 
@@ -83,5 +78,18 @@ def debug_output(props, prop):
     obs.obs_data_set_string(settings, "file", idle_image_path)
     obs.obs_source_update(source, settings);
 
+    obs.obs_data_release(settings)
+    obs.obs_source_release(source)
+
+
+def talk(props, prop):
+    if not pngtuber_source:
+        print("Select your PNGTuber source")
+
+    source = obs.obs_get_source_by_name(pngtuber_source)
+    settings = obs.obs_source_get_settings(source)
+    obs.obs_data_set_string(settings, "file", talking_image_path)
+    obs.obs_source_update(source, settings);
+    print(talking_image_path)
     obs.obs_data_release(settings)
     obs.obs_source_release(source)
